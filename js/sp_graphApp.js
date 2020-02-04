5ad5bad4cb7d07345d85026900c46f537c53186e
Luis <ld_ramirez27@outlook.com>
2020-02-04 18:11:26 -0500

    Nuevo grafo svg

--- js/sp_graphApp.js
+++ js/sp_graphApp.js
@@ -1,7 +1,8 @@
+/*
 const w = 500;
 const h = 500;
 
-const marco = d3.select("body")
+const marco = d3.select("#canvas-div")
   .append("canvas")
   .attr("width", w)
   .attr("height", h);
@@ -128,3 +129,147 @@ function drawLink(l) {
 }
 
 update();
+
+*/
+var svg = d3.select("svg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
+
+var r = 40;
+
+var graph = {
+  "nodes": [{
+      "id": "A",
+      "group": 1
+    },
+    {
+      "id": "B",
+      "group": 1
+    },
+    {
+      "id": "C",
+      "group": 3
+    },
+    {
+      "id": "D",
+      "group": 4
+    }
+  ],
+  "links": [{
+      "source": "A",
+      "target": "C",
+      "value": 1
+    },
+    {
+      "source": "B",
+      "target": "C",
+      "value": 3
+    },
+    {
+      "source": "C",
+      "target": "D",
+      "value": 2
+    },
+    {
+      "source": "D",
+      "target": "A",
+      "value": 1
+    }
+  ]
+}
+
+
+var link = svg.append("g")
+  .attr("class", "links")
+  .selectAll("line")
+  .data(graph.links)
+  .enter().append("line");
+
+var node = svg.append("g")
+  .attr("class", "nodes")
+  .selectAll("circle")
+  .data(graph.nodes)
+  .enter().append("circle")
+  .attr("r", r)
+  .call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended));
+
+
+/*
+//Para nodos rectangulares
+
+var node = svg.append("g")
+  .selectAll("rect")
+  .data(graph.nodes)
+  .enter().append("rect")
+  .attr("height", 50)
+  .attr("width", 100)
+  .attr("rx", 10)
+  .attr("ry", 10)
+  .call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended));
+*/
+
+node.append("title")
+  .text(function(d) {
+    return d.id;
+  });
+
+var simulation = d3.forceSimulation()
+  .force("link", d3.forceLink().id((d) => d.id))
+  .force("charge", d3.forceManyBody())
+  .force("collide", d3.forceCollide(r + 5))
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .on("tick", ticked);
+
+  simulation
+  .nodes(graph.nodes)
+  simulation
+  .force("link")
+  .links(graph.links);
+
+function ticked() {
+  link
+    .attr("x1", function(d) {
+      return d.source.x;
+    })
+    .attr("y1", function(d) {
+      return d.source.y;
+    })
+    .attr("x2", function(d) {
+      return d.target.x;
+    })
+    .attr("y2", function(d) {
+      return d.target.y;
+    });
+
+  //Para colocar el rectangulo cambiar al "cx" por "x", igual para "cy"
+  node
+    .attr("cx", function(d) {
+      return d.x;
+    })
+    .attr("cy", function(d) {
+      return d.y;
+    });
+}
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
